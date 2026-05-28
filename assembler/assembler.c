8eccc90f1d0d27ff43879564d5598e0b40bbee51
idobd11 <idobd11@gmail.com>
2026-05-28 16:59:54 +0300

    session 1

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,15 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+/* ============================================
+Structs and globals
+============================================== */
+
+ /* ============================================
+ Utility functions
+ ============================================== */
+
+ /* ============================================
+ SOpcode/register conversion
+ ============================================== */
+
+ /* ============================================
+ Labels / Symbols
+ ============================================== */
+
+ /* ============================================
+ .word/.array 
+ ============================================== */
+
+ /* ============================================
+ encoding functions 
+ ============================================== */
+
+ /* ============================================
+ Main
+ ============================================== */
+
 
 int main(int argc, char* argv[])
 {
+    FILE* input;
+    FILE* output;
+    char line[501];
+
     if (argc != 3) {
         printf("Usage: asm.exe program.asm memin.txt\n");
         return 1;
     }
 
-    printf("Assembler skeleton\n");
-    printf("Input asm file: %s\n", argv[1]);
-    printf("Output memin file: %s\n", argv[2]);
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+        printf("Error: cannot open input file\n");
+        return 1;
+    }
+
+    output = fopen(argv[2], "w");
+    if (output == NULL) {
+        printf("Error: cannot open output file\n");
+        fclose(input);
+        return 1;
+    }
+
+    while (fgets(line, 501, input) != NULL) {
+        printf("%s", line);
+    }
+
+    printf("Succesfully opened all files! Ready to parse...\n");
+
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
